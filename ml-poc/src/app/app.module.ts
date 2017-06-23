c5c46b4b38d0d9de9801d8c4df52ef09f9f6a59b
Shagun Singhal <shagun.singhal@synapse.com>
2017-06-23 17:21:04 +0530

    Routing changes

--- ml-poc/src/app/app.module.ts
+++ ml-poc/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {HttpModule } from '@angular/http';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { MdGridList, MdGridTile } from '@angular/material';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { MenuService } from './menu.service';
+import { ProductComponent } from './product/product.component';
+import { MembersComponent } from './members/members.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +18,26 @@ import { MenuService } from './menu.service';
     MenuComponent,
     MdGridList,
     MdGridTile,
+    ProductComponent,
+    MembersComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,    
+    BrowserAnimationsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {
+      path: 'member',
+      component: MembersComponent
+    },
+    {
+      path: 'product',
+      component: ProductComponent
+    }
+    
+
+
+    ])
   ],
   providers: [MenuService],
   bootstrap: [AppComponent]
