97199783329a8e37d48cbf0ccc45fbac690c1d89
Sanyam Bharti <sanyam.bharti@synapse.com>
2017-06-23 11:30:25 +0530

    resolved merge conflicts

--- ml-poc/src/app/ngx-table-component/ngx-table-component.component.ts
+++ ml-poc/src/app/ngx-table-component/ngx-table-component.component.ts
@@ -1,14 +1,24 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+
+import { Component, OnInit, Input, ViewContainerRef, ViewEncapsulation } from '@angular/core';
+import { Overlay, overlayConfigFactory } from 'angular2-modal';
+import { Modal, BSModalContext } from 'angular2-modal/plugins/bootstrap';
+
 
 @Component({
   selector: 'app-ngx-table-component',
   templateUrl: './ngx-table-component.component.html',
   styleUrls: ['./ngx-table-component.component.css'],
-  encapsulation: ViewEncapsulation.None
+
+  encapsulation: ViewEncapsulation.None,
+
+  providers: [Modal]
+
 })
 export class NgxTableComponentComponent implements OnInit {
   @Input() rows: Array<Object>;
-  constructor() { }
+  constructor(vcRef: ViewContainerRef, public modal: Modal, Overlay: Overlay) {
+    //  modal.defaultViewContainer = vcRef;
+  }
 
   ngOnInit() {
     console.log(this.rows);
@@ -18,6 +28,23 @@ export class NgxTableComponentComponent implements OnInit {
     console.log('here');
   }
 
-
+onClick() {
+    this.modal.alert()
+        .size('lg')
+        .showClose(true)
+        .title('A simple Alert style modal window')
+        .body(`
+            <h4>Alert is a classic (title/body/footer) 1 button modal window that 
+            does not block.</h4>
+            <b>Configuration:</b>
+            <ul>
+                <li>Non blocking (click anywhere outside to dismiss)</li>
+                <li>Size large</li>
+                <li>Dismissed with default keyboard key (ESC)</li>
+                <li>Close wth button click</li>
+                <li>HTML content</li>
+            </ul>`)
+        .open();
+  }
 
 }
