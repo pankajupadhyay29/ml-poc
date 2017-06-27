ad1df4271ad4822d2231bcaebfed039105907f58
shagunsinghal2207 <shagunsinghal2207@gmail.com>
2017-06-27 18:00:33 +0530

    icomoon changes

--- ml-poc/src/app/menu.service.ts
+++ ml-poc/src/app/menu.service.ts
@@ -9,11 +9,11 @@ export class MenuService {
     //This could be a service call for dynamic menus
     return [
       { id: 0, text: 'Home', name: 'home',  url:'/', isActive: true}, 
-      { id: 1, text: 'Databases', name: 'databases',  url:'/database'}, 
-      { id: 2, text: 'Group', name: 'Group',  url:'/'}, 
+      { id: 1, text: 'Databases', name: 'Database',  url:'/database'}, 
+      { id: 2, text: 'Group', name: 'group',  url:'/'}, 
       { id: 3, text: 'Hosts',  name: 'host',  url:'/'}, 
       { id: 4, text: 'Forests',  name: 'forest',  url:'/'}, 
-      { id: 5, text: 'Clusters',  name: 'Clusters',  url:'/'}, 
+      { id: 5, text: 'Clusters',  name: 'clusters',  url:'/'}, 
       { id: 6, text: 'Security',  name: 'security',  url:'/'}, 
     ];
   }
