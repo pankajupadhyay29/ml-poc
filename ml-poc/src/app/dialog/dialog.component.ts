49ddc2d20685ecd7bf4a5eb6fcf1c13a32b27ff2
Chetna Luthra <chetna.luthra@synapse.com>
2017-06-29 17:55:31 +0530

    Modal popup

--- ml-poc/src/app/dialog/dialog.component.ts
+++ ml-poc/src/app/dialog/dialog.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  OnInit, Input } from '@angular/core';
 import { MdDialog, MdDialogRef } from '@angular/material';
 import {MdGridListModule} from '@angular/material';
+import {DatabaseService} from '../database.service';
+import { MenuService } from '../menu.service';
 
 
 @Component({
@@ -9,10 +11,33 @@ import {MdGridListModule} from '@angular/material';
   styleUrls: ['./dialog.component.css']
 })
 export class DialogComponent implements OnInit {
+	temp: Array<Object>;
+	menuList =  [];
+  @Input() forestsList: Array<Object>;
 
-constructor(public dialogRef: MdDialogRef<DialogComponent>) { }
+  notVisible:boolean = true;
+  visible:boolean = false;
+constructor(public dialogRef: MdDialogRef<DialogComponent>, private dbService: DatabaseService, private menuService: MenuService) { }
   ngOnInit() {
+
   }
 
+  onClick() {
+  	this.dbService.getForests().subscribe(result=>{
+      this.forestsList = result;
+      this.temp = this.forestsList;
+      console.log(this.forestsList);
+      console.log('coming in dialog');
+
+    });
+  	this.visible = true;
+  	this.notVisible = false;
+  	   
+  }
+
+  	onBack() {
+  	this.visible = false;
+  	this.notVisible = true;
+  	}
   }
 
