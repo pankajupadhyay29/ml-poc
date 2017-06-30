0ee8621936758a77d0a29d0220654e8636f30fa0
Sanyam Bharti <sanyam.bharti@synapse.com>
2017-06-30 14:43:02 +0530

    all graphs

--- ml-poc/src/app/dashboard/dashboard.component.ts
+++ ml-poc/src/app/dashboard/dashboard.component.ts
@@ -11,6 +11,7 @@ import { ChartService } from "app/chart.service";
 export class DashboardComponent implements OnInit {
   widgets = [];
   chart = [];
+  AvailaibleChart = [];
   widgetsGraphs = [];
 
   chartOptions = {
@@ -50,188 +51,172 @@ export class DashboardComponent implements OnInit {
   constructor(private layout: LayoutService, private chart_service: ChartService) { }
 
   ngOnInit() {
-    // this.widgets = this.layout.getWidgetLayout();
-
     this.chart_service.getChartList().subscribe(result => {
-      this.widgets = this.layout.getWidgetLayout();
-      this.chart = result;
-      console.log('new', this.chart);
-      this.transform();
-      this.showBarChart();
-      for (let i = 0; i < this.widgets.length; i++) {
-      console.log('widgetgraphs in subscribe',this.widgetsGraphs[i]);
-      this.widgets[i]['chart'] = this.widgetsGraphs[i] || {};
-    }
-    console.log('widgets in subscribe',this.widgets)
-  });
+      this.chart_service.getAvailaibleChartList().subscribe(res => {
+        this.widgets = this.layout.getWidgetLayout();
+        this.chart = result;
+        this.transform();
+        this.AvailaibleChart = res;
+        this.transformAvailaibleChart();
+        this.showBarChart();
+        for (let i = 0; i < this.widgets.length; i++) {
+          console.log('widgetgraphs in subscribe', this.widgetsGraphs[i]);
+          this.widgets[i]['chart'] = this.widgetsGraphs[i] || {};
+        }
+      });
+      console.log('widgets in subscribe', this.widgets)
+    });
 
   }
 
-  showBarChart(){
+  transformAvailaibleChart() {
     let graphData = [];
-    let cOptions=this.barChartOptions;
+    let cOptions = this.chartOptions;
+
+    console.log('in transform', this.AvailaibleChart);
     graphData.push(cOptions);
-          graphData[0]['chart']['type'] = 'discreteBarChart';
-          graphData[0]['chart']['showValues'] = true;
-          graphData[0]['chart']['valueFormat'] = function (d) {
-             return d3.format(',.4f')(d);
-           };
-           graphData[0]['chart']['duration'] = 500;
-           graphData[0]['chart'].xAxis = {
-             axisLabel: 'X Axis'
-           }
-           graphData[0]['chart'].yAxis = {
-             axisLabel: 'Y Axis',
-             axisLabelDistance: -10
-           }
-           let bardata=this.createBarChartData();
-           graphData.push(bardata);
-           this.widgetsGraphs.push(graphData);
-           console.log('in showBarChart',this.widgetsGraphs);
+
+    graphData[0]['chart']['type'] = 'lineChart';
+    graphData[0]['chart']['useInteractiveGuideline'] = true;
+    graphData[0]['chart'].xAxis = { axisLabel: 'Time (ms)' }
+    graphData[0]['chart'].x = function (d) { return d.x; },
+      graphData[0]['chart'].y = function (d) { return d.y; },
+      graphData[0]['chart'].yAxis = {
+        axisLabel: 'No of Forests',
+        tickFormat: function (d) {
+          return d3.format('.02f')(d);
+        },
+        axisLabelDistance: -10
+      }
+    let data = this.createAvailaibleChartData();
+    graphData.push(data);
+    this.widgetsGraphs.push(graphData);
+
+  }
+
+  createAvailaibleChartData() {
+    let data = [];
+    let i = 0;
+    let linedata = {};
+    console.log('in avail', this.AvailaibleChart);
+    linedata['values'] = [];
+    this.AvailaibleChart.forEach(element => {
+      let tempdata = { x: 0, y: 0 };
+      tempdata.x = i;
+      tempdata.y = element.data;
+      linedata['values'].push(tempdata);
+      i += 1;
+    });
+    linedata['color'] = '#7777ff';
+    linedata['area'] = true;
+    data.push(linedata);
+    return data;
+  }
+
+  showBarChart() {
+    let graphData = [];
+    let cOptions = this.barChartOptions;
+    graphData.push(cOptions);
+    graphData[0]['chart']['type'] = 'discreteBarChart';
+    graphData[0]['chart']['showValues'] = true;
+    graphData[0]['chart']['valueFormat'] = function (d) {
+      return d3.format(',.4f')(d);
+    };
+    graphData[0]['chart']['duration'] = 500;
+    graphData[0]['chart'].xAxis = {
+      axisLabel: 'X Axis'
+    }
+    graphData[0]['chart'].yAxis = {
+      axisLabel: 'Y Axis',
+      axisLabelDistance: -10
+    }
+    let bardata = this.createBarChartData();
+    graphData.push(bardata);
+    this.widgetsGraphs.push(graphData);
+    console.log('in showBarChart', this.widgetsGraphs);
   }
 
 
   transform() {
     let graphData = [];
-    let cOptions=this.chartOptions;
+    let cOptions = this.chartOptions;
 
-          console.log('in transform', this.chart);
-         graphData.push(cOptions);
+    console.log('in transform', this.chart);
+    graphData.push(cOptions);
 
-          graphData[0]['chart']['type'] = 'lineChart';
-          graphData[0]['chart']['useInteractiveGuideline'] = true;
-          graphData[0]['chart'].xAxis = { axisLabel: 'Time (ms)' }
-          graphData[0]['chart'].x=function (d) { return d.x; },
-          graphData[0]['chart'].y=function (d) { return d.y; },
-          graphData[0]['chart'].yAxis = {
-            axisLabel: 'No of Forests',
-            tickFormat: function (d) {
-              return d3.format('.02f')(d);
-            },
-            axisLabelDistance: -10
-          }
-          
-          let data=this.createLineChartData();
-          graphData.push(data);
-          this.widgetsGraphs.push(graphData);
-      
-        // case 1: //graphData.push(this.chartOptions);
-        // //   graphData[0]['chart']['type'] = 'discreteBarChart';
-        // //   graphData[0]['chart']['showValues'] = true;
-        // //   graphData[0]['chart']['valueFormat'] = function (d) {
-        // //     return d3.format(',.4f')(d);
-        // //   };
-        // //   graphData[0]['chart']['duration'] = 500;
-        // //   graphData[0]['chart'].xAxis = {
-        // //     axisLabel: 'X Axis'
-        // //   }
-        // //   graphData[0]['chart'].yAxis = {
-        // //     axisLabel: 'Y Axis',
-        // //     axisLabelDistance: -10
-        // //   }
-        // //   // graphData.push(this.chart[0]['Data']);
-        // //   this.widgetsGraphs.push(graphData);
-        //   break;
-          // case 2:  graphData.push(this.chartOptions);
-          // graphData[0]['chart']['type'] = 'discreteBarChart';
-          // graphData[0]['chart']['showValues'] = true;
-          // graphData[0]['chart']['valueFormat'] = function (d) {
-          //    return d3.format(',.4f')(d);
-          //  };
-          //  graphData[0]['chart']['duration'] = 500;
-          //  graphData[0]['chart'].xAxis = {
-          //    axisLabel: 'X Axis'
-          //  }
-          //  graphData[0]['chart'].yAxis = {
-          //    axisLabel: 'Y Axis',
-          //    axisLabelDistance: -10
-          //  }
-          //  let bardata=this.createBarChartData();
-          //  graphData.push(bardata);
-          //  this.widgetsGraphs.push(graphData);
-          // break;
-     
-
-    
-    
+    graphData[0]['chart']['type'] = 'lineChart';
+    graphData[0]['chart']['useInteractiveGuideline'] = true;
+    graphData[0]['chart'].xAxis = { axisLabel: 'Time (ms)' }
+    graphData[0]['chart'].x = function (d) { return d.x; },
+      graphData[0]['chart'].y = function (d) { return d.y; },
+      graphData[0]['chart'].yAxis = {
+        axisLabel: 'No of Forests',
+        tickFormat: function (d) {
+          return d3.format('.02f')(d);
+        },
+        axisLabelDistance: -10
+      }
+    let data = this.createLineChartData();
+    graphData.push(data);
+    this.widgetsGraphs.push(graphData);
   }
-
-//     data: [
-  //       {
-  //         values: [{ x: 1, y: 5 }, { x: 5, y: 10 }, { x: 7, y: 8 }],      //values - represents the array of {x,y} data points
-  //         key: 'Sine Wave', //key  - the name of the series.
-  //         color: '#ff7f0e'  //color - optional: choose your own line color.
-  //       },
-  //       {
-  //         values: [{ x: 2, y: 10 }, { x: 5, y: 5 }, { x: 7, y: 11 }],      //values - represents the array of {x,y} data points,
-  //         key: 'Cosine Wave',
-  //         color: '#2ca02c'
-  //       },
-  //       {
-  //         values: [{ x: 3, y: 5 }, { x: 6, y: 11 }, { x: 7, y: 12 }],      //values - represents the array of {x,y} data points,
-  //         key: 'Another sine wave',
-  //         color: '#7777ff',
-  //         area: true      //area - set to true if you want this line to turn into a filled area chart.
-  //       }
-  //     ]
 
   createLineChartData() {
-    let data=[];
+    let data = [];
     console.log('in create', this.chart);
     this.chart.forEach(element => {
-          let linedata={};
-          linedata['values']=[];
-          for(let i=1;i<=element.data.length;i++)
-          {
-            let tempdata={x:0,y:0};
-            tempdata.x=i;
-            tempdata.y=element.data[i-1].data.length;
-            linedata['values'].push(tempdata);
-          }
-          linedata['key']=element.name;
-          linedata['color']='#7777ff';
-          data.push(linedata);
+      let linedata = {};
+      linedata['values'] = [];
+      for (let i = 1; i <= element.data.length; i++) {
+        let tempdata = { x: 0, y: 0 };
+        tempdata.x = i;
+        tempdata.y = element.data[i - 1].data.length;
+        linedata['values'].push(tempdata);
+      }
+      linedata['key'] = element.name;
+      linedata['color'] = '#7777ff';
+      data.push(linedata);
     });
-          return data;
-    
+    return data;
+
   }
 
 
-  createBarChartData(){
+  createBarChartData() {
     let data;
-    data= [
-        {
-          key: "Cumulative Return",
-          values: [
-            {
-              "label": "A",
-              "value": 29.765957771107
-            },
-            {
-              "label": "B",
-              "value": 0
-            },
-            {
-              "label": "C",
-              "value": 32.807804682612
-            },
-            {
-              "label": "D",
-              "value": 196.45946739256
-            },
-            {
-              "label": "E",
-              "value": 0.19434030906893
-            },
-            {
-              "label": "F",
-              "value": 98.079782601442
-            },
-          ]
-        }
-      ]
+    data = [
+      {
+        key: "Cumulative Return",
+        values: [
+          {
+            "label": "A",
+            "value": 29.765957771107
+          },
+          {
+            "label": "B",
+            "value": 0
+          },
+          {
+            "label": "C",
+            "value": 32.807804682612
+          },
+          {
+            "label": "D",
+            "value": 196.45946739256
+          },
+          {
+            "label": "E",
+            "value": 0.19434030906893
+          },
+          {
+            "label": "F",
+            "value": 98.079782601442
+          },
+        ]
+      }
+    ]
 
-      return data;
+    return data;
   }
 
   getWidgetClasses(widget) {
