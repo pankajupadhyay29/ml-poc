64f54944af10966b27f11727d1c1a9bb2db2a320
Sanyam Bharti <sanyam.bharti@synapse.com>
2017-07-03 16:44:17 +0530

    chart changes

--- ml-poc/src/app/db-availability-trend/db-availability-trend.component.ts
+++ ml-poc/src/app/db-availability-trend/db-availability-trend.component.ts
@@ -10,13 +10,14 @@ export class DbAvailabilityTrendComponent implements OnInit {
 
   chartData: any;
   options: any;
+  interval:any;
   widgets = [];
   chart = [];
   widgetsGraphs = [];
 
   chartOptions = {
     chart: {
-      type: '',
+      type: 'lineChart',
       height: 300,
       margin: {
         top: 20,
@@ -24,49 +25,37 @@ export class DbAvailabilityTrendComponent implements OnInit {
         bottom: 50,
         left: 55
       },
-      x: function (d) { return d.lable; },
-      y: function (d) { return d.value; },
-      xAxis: {},
-      yAxis: {}
-    }
-  };
-
-  constructor(private chart_service: ChartService) { }
-
-  ngOnInit() {
-    this.chart_service.getAvailaibleChartList().subscribe(result => {
-      console.log(result);
-      this.chart = result;
-      this.transformAvailaibleChart();
-    });
-  }
-
-  transformAvailaibleChart() {
-    let graphData = [];
-    let cOptions = this.chartOptions;
-
-    graphData.push(cOptions);
-
-    graphData[0]['chart']['type'] = 'lineChart';
-    graphData[0]['chart']['useInteractiveGuideline'] = true;
-    graphData[0]['chart'].xAxis = { axisLabel: 'Time (ms)' }
-    graphData[0]['chart'].x = function (d) { return d.x; },
-      graphData[0]['chart'].y = function (d) { return d.y; },
-      graphData[0]['chart'].yAxis = {
+      useInteractiveGuideline: true,
+      x: function (d) { return d.x; },
+      y: function (d) { return d.y; },
+      xAxis: { axisLabel: 'Time (ms)' },
+      yAxis: {
         axisLabel: 'NO OF REQUESTS',
         tickFormat: function (d) {
           return d3.format('.02f')(d);
         },
         axisLabelDistance: -10
       }
+    }
+  };
 
-    let data = this.createAvailaibleChartData();
-    graphData.push(data);
-     this.options = graphData[0];
-     this.chartData = graphData[1];
 
+  constructor(private chart_service: ChartService) { }
+
+  ngOnInit() {
+     this.loadService = this.loadService.bind(this);
+    this.loadService();
+    this.interval = setInterval(this.loadService, 20 * 1000);
+    
   }
 
+  loadService(){
+    this.chart_service.getAvailaibleChartList().subscribe(result => {
+      this.chart = result;
+      this.options = this.chartOptions;
+      this.chartData = this.createAvailaibleChartData();
+    });
+  }
 
   createAvailaibleChartData() {
     let data = [];
@@ -76,7 +65,12 @@ export class DbAvailabilityTrendComponent implements OnInit {
     this.chart.forEach(element => {
       let tempdata = { x: 0, y: 0 };
       tempdata.x = i;
-      tempdata.y = element.data;
+      if (element.data != undefined) {
+        tempdata.y = element.data.UnavialableDB;
+      }
+      else {
+        tempdata.y = 0;
+      }
       linedata['values'].push(tempdata);
       i += 3;
     });
@@ -84,5 +78,8 @@ export class DbAvailabilityTrendComponent implements OnInit {
     linedata['area'] = true;
     data.push(linedata);
     return data;
+  }
+   ngOnDestroy() {
+    clearInterval(this.interval);
   }
 }
--- ml-poc/src/app/memory-uses-trend/memory-uses-trend.component.ts
+++ ml-poc/src/app/memory-uses-trend/memory-uses-trend.component.ts
@@ -12,7 +12,10 @@ export class MemoryUsesTrendComponent implements OnInit {
   chart = [];
   barChartOptions = {
     chart: {
-      type: '',
+      type: 'discreteBarChart',
+      valueFormat:function (d) {
+      return d3.format(',.4f')(d);
+    },
       height: 300,
       margin: {
         top: 20,
@@ -20,43 +23,26 @@ export class MemoryUsesTrendComponent implements OnInit {
         bottom: 50,
         left: 55
       },
+      duration:500,
       x: function (d) { return d.label; },
       y: function (d) { return d.value; },
-      xAxis: {},
-      yAxis: {}
+      xAxis: {
+      axisLabel: 'DATABASES'
+    },
+      yAxis: {
+      axisLabel: 'MEMORY',
+      axisLabelDistance: -10
+    }
     }
   };
 
   constructor() { }
 
   ngOnInit() {
-    this.showBarChart()
+    this.options = this.barChartOptions;
+    this.chartData = this.createBarChartData();
   }
-
-  showBarChart() {
-    let graphData = [];
-    let cOptions = this.barChartOptions;
-    graphData.push(cOptions);
-    graphData[0]['chart']['type'] = 'discreteBarChart';
-    // graphData[0]['chart']['showValues'] = true;
-    graphData[0]['chart']['valueFormat'] = function (d) {
-      return d3.format(',.4f')(d);
-    };
-    graphData[0]['chart']['duration'] = 500;
-    graphData[0]['chart'].xAxis = {
-      axisLabel: 'DB Name'
-    }
-    graphData[0]['chart'].yAxis = {
-      axisLabel: 'MEMORY',
-      axisLabelDistance: -10
-    }
-    let bardata = this.createBarChartData();
-    graphData.push(bardata);
-    this.options = graphData[0];
-    this.chartData = graphData[1];
-
-  }
-
+  
   createBarChartData() {
     let data;
     data = [
@@ -64,29 +50,29 @@ export class MemoryUsesTrendComponent implements OnInit {
         key: "Cumulative Return",
         values: [
           {
-            "label": "DB1",
+            "label": "App-Server",
             "value": 20
           },
           {
-            "label": "DB2",
+            "label": "Documents",
             "value": 10
           },
           {
-            "label": "DB3",
+            "label": "Extensions",
             "value": 50
           },
           {
-            "label": "DB4",
+            "label": "Fab",
             "value": 70
           },
-          {
-            "label": "DB5",
-            "value": 90
-          },
-          {
-            "label": "DB6",
-            "value": 40
-          },
+          // {
+          //   "label": "Last-Login",
+          //   "value": 90
+          // },
+          // {
+          //   "label": "Meter",
+          //   "value": 40
+          // },
         ]
       }
     ]
